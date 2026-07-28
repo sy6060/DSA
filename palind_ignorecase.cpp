592b62e586e271d0ef2fd11de47bcfcfb4417e27
Sanvi Yadav <sanvi.25b01010431@abes.ac.in>
2026-07-28 12:06:43 +0530

    using stack

--- palind_ignorecase.cpp
+++ palind_ignorecase.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 int main(){
     string s;
-    cin>>s;
-    string p=""; //contains string with only alphabets and in lower case
+    getline(cin,s);         //to enter string with spaces
+    string p="";
     int n=s.length();
     stack<char> t;
-    for(int i=0;i<n;i++){//pushing string in stack
-    s[i]=tolower(s[i]);
-       if((s[i]>64&&s[i]<91)||(s[i]>96&&s[i]<123)){ 
-        t.push(s[i]); //creating stack
-       p+=s[i];}//creating string with only alpjabets and in lower case
+    for(int i=0;i<n;i++){   //pushing string in stack
+    if(s[i]==' ')continue;   //ignore spaces
+    s[i]=tolower(s[i]);     //ignore case and convert to all lowercase
+       if((s[i]>64&&s[i]<91)||(s[i]>96&&s[i]<123)){ t.push(s[i]);    //push onlu alphabets
+       p+=s[i];}    //making clean string alongside
     }string rev="";
-    for(size_t i=0;i<p.length();i++){//reversing the string and popping stack
+    for(int i=0;i<p.length();i++){       //popping string from stack
 rev+=t.top();
 t.pop();
     }
