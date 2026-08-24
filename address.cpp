02d73f9abfd4a38616adcdef0124fbae7eb8d487
Sanvi Yadav <sanvi.25b01010431@abes.ac.in>
2026-08-24 18:09:37 +0530

    passing an obj as arg

---
+++ address.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Student{
+  public:
+  int m;
+  void change(Student *s){
+      s->m=55;
+      cout<<s->m<<endl;
+  }
+};
+int main() {
+ Student s;
+ s.m=100;
+ s.change(&s);
+ cout<<s.m;
+ return 0;
+}
